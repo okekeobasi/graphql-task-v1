3e41e3d0b812a8949e29d800039679c4702fd990
Christopher Obasi <okekeobasi@gmail.com>
2019-09-02 11:41:46 +0100

    mason mount

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  if Rails.env.development? || Rails.env.staging?
+  if Rails.env.development? || Rails.env.staging? || Rails.env.production?
     # GraphQL Interface
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
-
-  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
 
   post '/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
